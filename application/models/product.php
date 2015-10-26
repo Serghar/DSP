4aa5fe7adb9bafef797d6e67d87975b333e92e98
Dylan Sharkey <Serghar12@gmail.com>
2015-10-26 15:21:30 -0700

    added the product add function

--- application/models/product.php
+++ application/models/product.php
@@ -5,6 +5,20 @@ class Product extends CI_Model {
          $query = "SELECT * FROM products"; 
          return $this->db->query($query)->result_array();
      }
+
+     //insert the new product into database
+     public function new_product($post)
+     {
+     	$query = "INSERT INTO products (name, description, price, created_at, updated_at) VALUES (?, ?, ?, NOW(), NOW())";
+     	$values = array($post['name'], $post['description'], $post['price']);
+     	$this->db->query($query, $values);
+     }
+
+     //Should be used for validation of post data for the product
+     public function validate_product($post)
+     {
+
+     }
      
 }
 ?>
